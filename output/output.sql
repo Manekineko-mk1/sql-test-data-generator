334f2fd1df43caa8e4241880db9b38c53979b693
Manekineko-mk1 <HoHoMrChan@gmail.com>
2025-06-04 17:17:20 -0400

    Update .gitignore

--- output/output.sql
+++ output/output.sql
@@ -1,8 +1,11 @@
-INSERT INTO test_schema_1 (schl_id, TYP_VVCF, weight, created_date, amount) VALUES (141, 'IX', 237, '2021-08-31', 729.35);
-INSERT INTO test_schema_1 (schl_id, TYP_VVCF, weight, created_date, amount) VALUES (684, 'Pl', 40, '2020-12-22', 158.01);
-INSERT INTO test_schema_1 (schl_id, TYP_VVCF, weight, created_date, amount) VALUES (315, 'MG', 563, '2020-10-04', 368.89);
-****
-INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (727, 'IX', 237, TRUE, 'ZKpyWXWQfL');
-INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (567, 'Pl', 40, TRUE, 'glzwcWMAMS');
-INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (704, 'MG', 563, TRUE, 'suUYyBUgjn');
+INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (272, 'hC', 159, FALSE, 'vzCtXGMYpn');
+INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (966, 'or', 311, FALSE, 'whQSrVDyMc');
+INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (459, 'EX', 836, FALSE, 'faTFpQoHiJ');
+INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (984, 'xx', 92, FALSE, 'xYwxgMysmy');
+INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (146, 'ij', 912, TRUE, 'BPnZHApqPd');
+INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (339, 'FX', 650, TRUE, 'qGjeOrGDTO');
+INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (479, 'kb', 299, TRUE, 'QkGFxgXIvO');
+INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (291, 'ur', 147, TRUE, 'dRWgiwtcwA');
+INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (466, 'WW', 501, TRUE, 'nBGByspVjY');
+INSERT INTO test_schema_2 (ref_id, TYP_VVCF, weight, status, code) VALUES (620, 'dT', 130, TRUE, 'WoatOrtcUj');
 ****
